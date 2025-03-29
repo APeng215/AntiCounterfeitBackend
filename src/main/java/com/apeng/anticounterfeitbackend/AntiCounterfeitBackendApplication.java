d774a104f84922cdee0f08fe6efeeed8a8982589
A_Peng215 <976310604@qq.com>
2025-03-29 14:50:43 +0800

    Initial commit

---
+++ src/main/java/com/apeng/anticounterfeitbackend/AntiCounterfeitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.apeng.anticounterfeitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiCounterfeitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiCounterfeitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apeng/anticounterfeitbackend/AntiCounterfeitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apeng.anticounterfeitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiCounterfeitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
