b685764bb1139be91e3248cd29b7ef4484fcd0ae
A_Peng215 <976310604@qq.com>
2025-03-29 18:28:13 +0800

    Optimize imports

--- src/main/java/com/apeng/anticounterfeitbackend/entity/Goods.java
+++ src/main/java/com/apeng/anticounterfeitbackend/entity/Goods.java
@@ -2,7 +2,6 @@ package com.apeng.anticounterfeitbackend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
